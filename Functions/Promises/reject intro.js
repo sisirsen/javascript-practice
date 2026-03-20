f034d2dcba23d1d4326c13b2e0124810a6d32d3f
SISIR SEN <sisirsen22122004@gmail.com>
2026-03-20 22:29:35 +0530

    .then .catch

--- Functions/Promises/reject intro.js
+++ Functions/Promises/reject intro.js
@@ -1,8 +1,17 @@
 const promise=new Promise(function(resolve,reject){
   setTimeout(()=>{
-    let error=true;
+    let error=false;
     if (!error) {
-      resolve({username:"sisir",})
+      resolve({username:"sisir",password:"12345"})
+    }else{
+      reject("ERROR:whats on your mind")
     }
   },2000)
 })
+promise.then(function(user){
+  console.log(user);
+  return user.username;
+  
+}).then((username)=>console.log(username)
+).catch((error)=>console.log(error)
+)
