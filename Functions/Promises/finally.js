cf34cede03e7fdb99762539239af12a18a9f2cea
SISIR SEN <sisirsen22122004@gmail.com>
2026-03-20 22:46:59 +0530

    finally

---
+++ Functions/Promises/finally.js
@@ -0,0 +1,18 @@
+const promise=new Promise(function(resolve,reject){
+  setTimeout(()=>{
+    let error=false;
+    if (!error) {
+      resolve({username:"sisir",password:"12345"})
+    }else{
+      reject("ERROR:whats on your mind")
+    }
+  },2000)
+})
+promise.then(function(user){
+  console.log(user);
+  return user.username;
+  
+}).then((username)=>console.log(username)
+).catch((error)=>console.log(error)
+).finally(()=>console.log("your promise is done i dont know what is done  😁  but its done ")
+)
