a5d5ddb324413f3e5064e8ac7205ab82a6b3562d
SISIR SEN <sisirsen22122004@gmail.com>
2026-04-01 23:23:33 +0530

    JSON in objects

---
+++ Objects/JSON in objects.js
@@ -0,0 +1,11 @@
+const user = {
+  name: "Sisir",
+  age: 21
+};
+
+const jsonData = JSON.stringify(user);
+
+console.log(jsonData);
+
+//The difference is in object {name:"sisir",age:21}.
+                  //in Json {"name":"sisir","age":21}
