b82cd5a18caf7da043bc20067e5b121f6890c7f5
SISIR SEN <sisirsen22122004@gmail.com>
2026-04-01 22:41:30 +0530

    objects with functions

---
+++ Objects/objects with functions.js
@@ -0,0 +1,10 @@
+const car = {
+  color: "red",
+  speed: 120,
+
+  drive: function() {
+    console.log("Car is driving");
+  }
+};
+
+car.drive();  //nothing just assigning the function as the value.
