d193f8da46d403c3a25038a92b84da784f1b0fc4
SISIR SEN <sisirsen22122004@gmail.com>
2026-04-01 23:15:26 +0530

    arrays of objects

---
+++ Objects/arrays of objects.js
@@ -0,0 +1,12 @@
+const users = [
+  { name: "Sisir", age: 21 },
+  { name: "Rahul", age: 23 },
+  { name: "Amit", age: 20 }
+];
+
+console.log(users[0]);
+
+let newUser=[{name:"kalu",age:22,},...users]
+
+console.log(newUser);
+
