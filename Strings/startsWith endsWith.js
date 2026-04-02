795341d4dac9718a827feb58431d16bd8f5befb7
SISIR SEN <sisirsen22122004@gmail.com>
2026-04-02 22:22:34 +0530

    startsWith() and endsWith() in string

--- Strings/startsWith endsWith.js
+++ Strings/startsWith endsWith.js
@@ -4,9 +4,11 @@ let newStr=str.startsWith("hi");
 let newStr1=str.startsWith("Sisir");
 let newStr2=str.endsWith("hi");
 let newStr3=str.endsWith("Sisir");
+let newStr4=str[0].endsWith("h");
 
 console.log(newStr);
 console.log(newStr1);
 console.log(newStr2);
 console.log(newStr3);
+console.log(newStr4);
 
