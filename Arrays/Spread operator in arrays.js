47a2bbbcdc3567e0a355669b43ab944b7ac322c7
SISIR SEN <sisirsen22122004@gmail.com>
2026-04-03 20:01:12 +0530

    spread operator

--- Arrays/Spread operator in arrays.js
+++ Arrays/Spread operator in arrays.js
@@ -4,3 +4,11 @@ arr[1]=5;
 
 let newArr=[...arr]
 console.log(newArr);
+
+
+
+let arr2=[1,2,3,4,5,6,7,8,9]
+let newArr2=[arr[1]=5,...arr]
+console.log(newArr);
+
+//both are same just declaration difference.
