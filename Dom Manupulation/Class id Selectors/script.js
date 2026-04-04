fda22f514e70ef5bddf1d2c4320ff19797a073c6
SISIR SEN <sisirsen22122004@gmail.com>
2026-04-04 19:36:17 +0530

    Selectors in dom

---
+++ Dom Manupulation/Class id Selectors/script.js
@@ -0,0 +1,33 @@
+// let boxes=document.getElementsByClassName("box") 
+// boxes[2].style.backgroundColor="red";class selector;
+
+
+// let bz=document.getElementById("bz");
+// bz.style.color="green" specific id selector;
+
+
+// let bz = document.querySelector("#bz");
+// bz.style.backgroundColor="pink" 
+
+
+// let bz=document.querySelector(".container");
+// bz.style.backgroundColor="pink"
+// bz.style.color="red"
+
+
+// let bg=document.querySelectorAll(".box");
+// bg.forEach((el)=>{
+//   return el.style.backgroundColor="pink"
+// })    here queryselectorAll select all elements who have class box and give them color pink.
+
+
+
+// Factorial of a given No.
+// let a=6;
+// let factorial=1;
+
+// for (let i = a; i >=1; i--) {
+//   factorial=factorial*i
+  
+// }
+// console.log(`factorial of ${a} is ${factorial}`);
