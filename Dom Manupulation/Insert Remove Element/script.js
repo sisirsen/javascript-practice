85023b420daad4741d71a15c48ecb50d556f36ef
SISIR SEN <sisirsen22122004@gmail.com>
2026-04-05 20:40:58 +0530

    Element insert/add and manupulation

---
+++ Dom Manupulation/Insert Remove Element/script.js
@@ -0,0 +1,52 @@
+// let senBabu=document.querySelector(".sen");
+
+// console.log(senBabu.innerHTML); // shows the inner content without the element.
+
+// console.log(senBabu.outerHTML); //  shows the outer content with the element.
+
+// console.log(senBabu.textContent);  // shows the text 
+
+// console.log(senBabu.tagName); // shows the wrapper tag name 
+
+// let now =senBabu.innerHTML="hello bro"; // manupulates the inner text and change the text
+// console.log(now);
+
+//     // Create Elements in dom
+
+//     let newEl=document.createElement("div")
+//     newEl.innerText="bro i am sisir"  
+//     newEl.style.color="blue"
+//     newEl.className=".senbabu"
+//     document.body.append(newEl)
+    
+// //using prepend
+//     let newEl2=document.createElement("div");
+//     newEl2.innerText="hi bro how are you";
+//     document.body.prepend(newEl2);
+
+
+// //using before
+//     let newEl3=document.createElement("div");
+//     newEl3.innerText="you are good";
+//     document.querySelector(".sen").before(newEl3)
+
+// //using after
+
+//     let newEl4=document.createElement("div");
+//     newEl4.innerText="you are good at after";
+//     document.querySelector(".sen").after(newEl4)
+
+//
+
+let newEl5=document.querySelector(".botul");
+
+// newEl5.insertAdjacentHTML("beforeend","<p> i am a bot1 </p>")
+// newEl5.insertAdjacentHTML("afterend","<p> i am a bot2 </p>")
+// newEl5.insertAdjacentHTML("beforebegin","<p> i am a bot3 </p>")
+// newEl5.insertAdjacentHTML("afterbegin","<p> i am a bot4 </p>")
+
+
+
+
+
+
