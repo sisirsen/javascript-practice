3d5635be46a49e0ff5354859809136aa2fa36d60
SISIR SEN <sisirsen22122004@gmail.com>
2026-04-07 00:02:55 +0530

    Float management with math in js

--- Math Built INs/float.js
+++ Math Built INs/float.js
@@ -1,4 +1,10 @@
-console.log(Math.round(4.4));// 5 //if after ._ is greater than 5 then the next number will be printed.
+console.log(Math.round(4.4)); //prints the nearest value
 
 
-console.log(Math.floor(4.9));//it doesnot care about ._ ;
+console.log(Math.floor(4.9));//it always does down from the element
+
+console.log(Math.ceil(4.2));// prints always up element
+
+console.log(Math.trunc(4.5)); // cuts the after ._;
+
+
